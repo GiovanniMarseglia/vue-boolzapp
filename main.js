4c0e1eae80a80262ed592a83813adeee68e6a4d7
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-01-30 11:24:15 +0100

    created file

---
+++ main.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+
+
+
+
+
+        message: 'Hello Vue!'
+      }
+    },
+    methods: {
+      
+    },
+  }).mount('#app')
